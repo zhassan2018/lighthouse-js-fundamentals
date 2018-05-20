8a3939ab1fc9b793808c3c07adafad0feaf12562
Zohaib <zohaib.hassan@mail.mcgill.ca>
2018-05-20 22:31:12 +0000

    Adds conditionals exercises

---
+++ go-outside.js
@@ -0,0 +1,11 @@
+var cold = false;
+
+if (cold){
+	console.log("Make sure you pick out a scarf")
+}
+
+else {
+	console.log("Short sleeves are fine.")
+}
+
+console.log("Now you're ready to go outside")
---
+++ howManyHundreds.js
@@ -0,0 +1,12 @@
+function howManyHundreds (number)
+{
+	var result = number/100;
+	result = Math.floor(result);
+	return result;
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
