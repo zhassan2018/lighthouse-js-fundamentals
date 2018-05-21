f747d0ab8be1e7f5d118db107cbe1aff63163020
Zohaib <zohaib.hassan@mail.mcgill.ca>
2018-05-21 01:31:01 +0000

    Adds looping exercises

---
+++ ingredients.js
@@ -0,0 +1,15 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+var i = 0;
+while (i < ingredients.length ){
+	console.log(ingredients[i]);
+	i+=1;
+}
+
+for (i = 0; i < ingredients.length; i++){
+	console.log(ingredients[i])
+}
+
+for (i = ingredients.length - 1; i >= 0; i--){
+	console.log(ingredients[i])
+}
+
